df70b8a644d12d95add787fd3282a952ba7b071c
dep <1649743146@qq.com>
2024-01-07 15:03:22 +0800

    feat: 新增WebWorkers

---
+++ HTML5/WebWorker/SharedWorkers/worker.js
@@ -0,0 +1,13 @@
+// sharedWorker.js
+let connections = [];
+
+self.onconnect = function(event) {
+  let port = event.ports[0];
+  connections.push(port);
+
+  port.onmessage = function(event) {
+    for (let i = 0; i < connections.length; i++) {
+      connections[i].postMessage(event.data);
+    }
+  }
+};
