6474b2872a8b323386ca6e49203e32522c1ca274
dep <1649743146@qq.com>
2024-02-16 10:23:48 +0800

    feat: 循环渲染

--- 面试题/Vue面试题/vue2-demo/src/App.vue
+++ 面试题/Vue面试题/vue2-demo/src/App.vue
@@ -5,12 +5,14 @@
     <WatchDemo></WatchDemo>
     <ClassDemo></ClassDemo>
     <ConditionDemo></ConditionDemo>
+    <ListDemo></ListDemo>
   </div>
 </template>
 <script>
 import ClassDemo from './views/基本使用/ClassDemo.vue'
 import ComputedDemo from './views/基本使用/ComputedDemo.vue'
 import ConditionDemo from './views/基本使用/ConditionDemo.vue'
+import ListDemo from './views/基本使用/ListDemo.vue'
 import TqlDemo from './views/基本使用/TqlDemo.vue'
 import WatchDemo from './views/基本使用/WatchDemo.vue'
 export default {
@@ -20,7 +22,8 @@ export default {
     ComputedDemo,
     WatchDemo,
     ClassDemo,
-    ConditionDemo
+    ConditionDemo,
+    ListDemo
   }
 }
 </script>
