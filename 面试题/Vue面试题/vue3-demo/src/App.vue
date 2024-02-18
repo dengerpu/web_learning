9b3bd3b003402f376c73d21635beaec3cfd64827
dep <1649743146@qq.com>
2024-02-18 19:50:43 +0800

    feat: Vue3生命周期

---
+++ 面试题/Vue面试题/vue3-demo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <life-cycles></life-cycles>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import LifeCycles from './components/LifeCycles.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    LifeCycles
+  }
+}
+</script>
