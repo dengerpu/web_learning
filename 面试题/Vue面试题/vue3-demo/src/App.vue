bab76ef25c07765420934dd588ce677cfd78a00d
dep <1649743146@qq.com>
2024-02-22 12:31:34 +0800

    feat: toRef toRefs

--- 面试题/Vue面试题/vue3-demo/src/App.vue
+++ 面试题/Vue面试题/vue3-demo/src/App.vue
@@ -2,6 +2,8 @@
   <life-cycles></life-cycles>
   <ref></ref>
   <ref-template></ref-template>
+  <toRef></toRef>
+  <toRefs></toRefs>
 </template>
 
 <script>
@@ -9,6 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
 import LifeCycles from './components/LifeCycles.vue'
 import Ref from './components/Ref.vue'
 import RefTemplate from './components/RefTemplate.vue'
+import toRef from './components/toRef.vue'
+import toRefs from './components/toRefs.vue'
 
 export default {
   name: 'App',
@@ -16,7 +20,9 @@ export default {
     HelloWorld,
     LifeCycles,
     Ref,
-    RefTemplate
+    RefTemplate,
+    toRef,
+    toRefs
   }
 }
 </script>
---
+++ 面试题/Vue面试题/vue3-demo/src/components/toRefs.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>toRefs demo {{ age }} {{ name }}</p>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+export default {
+  name: "ToRefs",
+  setup() {
+    const state = reactive({
+      age: 20,
+      name: "张三",
+    });
+
+    // const stateAsRefs = toRefs(state); // 将响应式对象，变成普通对象
+
+    // const { age: ageRef, name: nameRef } = stateAsRefs // 每个属性，都是 ref 对象
+    // return {
+    //     ageRef,
+    //     nameRef
+    // }
+
+    setTimeout(() => {
+      state.age = 25;
+    }, 1500);
+
+    return {
+      ...toRefs(state),
+      // ...state // 这样会丢失响应式
+    };
+  },
+};
+</script>
