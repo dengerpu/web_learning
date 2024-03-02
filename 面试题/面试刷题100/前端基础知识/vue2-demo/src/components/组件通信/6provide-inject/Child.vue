d1eb42b91917391759202c070da13927b0aa704b
dep <1649743146@qq.com>
2024-03-02 12:52:14 +0800

    update: 更新inject

--- 面试题/面试刷题100/前端基础知识/vue2-demo/src/components/组件通信/6provide-inject/Child.vue
+++ 面试题/面试刷题100/前端基础知识/vue2-demo/src/components/组件通信/6provide-inject/Child.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>Child子组件，msg:{{msg2()}}</div>
+  <div>Child子组件，msg:{{ msg2() }}</div>
 </template>
 
 <script>
 export default {
   // inject: ['msg'],
-  inject: ['msg1', 'msg2'],
+  // inject: ['msg1', 'msg2'],
+  inject: {
+    msg: {
+      default: "使用默认值",
+    },
+    msg1: "msg1",
+    msg2: "msg2",
+  },
   created() {
-    console.log(this.msg, this.msg1)
-  }
-}
+    console.log(this.msg, this.msg1);
+  },
+};
 </script>
