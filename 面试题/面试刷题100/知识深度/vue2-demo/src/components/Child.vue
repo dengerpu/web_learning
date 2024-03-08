246725fccbe41f4db0d160cbb3d4908a3d7c35f1
dep <1649743146@qq.com>
2024-03-08 12:18:10 +0800

    feat: 父子组件生命周期演示

---
+++ 面试题/面试刷题100/知识深度/vue2-demo/src/components/Child.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>子组件</div>
+    <p>父组件传递过来的数据:{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChildDemo',
+  props: ['msg'],
+  beforeCreate() {
+    // 这里获取不到this.msg，获取会报错
+    console.log('子beforeCreate')
+  },
+  created() {
+    console.log('子created', this.msg)
+  },
+  beforeMount() {
+    console.log('子beforeMount')
+  },
+  mounted() {
+    console.log('子mounted')
+  },
+  beforeUpdate() {
+    console.log('子beforeUpdate')
+  },
+  updated() {
+    console.log('子updated')
+  },
+  activated() {
+    console.log('子activated')
+  },
+  deactivated() {
+    console.log('子deactivated')
+  },
+  beforeDestroy() {
+    console.log('子beforeDestroy')
+  },
+  destroyed() {
+    console.log('子destroyed')
+  },
+  errorCaptured() {
+    console.log('子errorCaptured')
+  }
+}
+</script>
---
+++ 面试题/面试刷题100/知识深度/vue2-demo/src/components/Parent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div ref="div">父组件</div>
+    <input type="text" v-model="msg">
+    <div>{{newMsg}}</div>
+    <hr>
+    <child :msg="msg"></child>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+  name: 'ParentDemo',
+  components: {
+    Child
+  },
+  data() {
+    return {
+      msg: '父组件的数据'
+    }
+  },
+  beforeCreate() {
+    console.log('父beforeCreate', this.msg)
+  },
+  created() {
+    console.log('父created', this.msg)
+  },
+  beforeMount() {
+    console.log('父beforeMount', this.$refs.div)
+  },
+  mounted() {
+    console.log('父mounted', this.$refs.div)
+  },
+  beforeUpdate() {
+    console.log('父beforeUpdate', this.$refs.div.innerHTML)
+  },
+  updated() {
+    console.log('父updated', this.$refs.div.innerHTML)
+  },
+  activated() {
+    console.log('父activated')
+  },
+  deactivated() {
+    console.log('父deactivated')
+  },
+  beforeDestroy() {
+    console.log('父beforeDestroy')
+  },
+  destroyed() {
+    console.log('父destroyed')
+  },
+  errorCaptured() {
+    console.log('父errorCaptured')
+  },
+  computed: {
+    newMsg() {
+      return this.msg + 'computed'
+    }
+  },
+  watch: {
+    msg: {
+      handler(newVal, oldVal) {
+        console.log('父watch', newVal, oldVal)
+      },
+      immediate: true
+    }
+  }
+}
+</script>
