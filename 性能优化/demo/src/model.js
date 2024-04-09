735e3d88b14686e7d15410c874ba50f35ded14c0
dep <1649743146@qq.com>
2024-04-09 20:57:10 +0800

    feat: 高频事件防抖

--- 性能优化/demo/src/model.js
+++ 性能优化/demo/src/model.js
@@ -1,22 +1,42 @@
 const model = [
     {
         name: `Amy的城堡纸盒`,
-        image: `https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=94996843,1878572032&fm=26&gp=0.jpg`,
+        image: `https://inews.gtimg.com/om_bt/OjPq2cnMN_-ivDKjxpCZ2kk_ab8YC5VMnL-0pQ21fUvd4AA/1000`,
         body: '全新, 隐蔽性好',
     },
     {
         name: 'Baddy的饼干盒',
-        image: `https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1125791419,1561105245&fm=26&gp=0.jpg`,
+        image: `https://pic.rmb.bdstatic.com/bjh/914b8c0f9814b14c5fedeec7ec6615df5813.jpeg`,
         body: '适合瘦身塑行',
     },
     {
         name: 'Corney的飞行纸盒',
-        image: `https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592014377244&di=da390556e384c148a63692fee68b6251&imgtype=0&src=http%3A%2F%2Fpic4.zhimg.com%2Fv2-0d4d16b3aa0e23d656bc1a94d7dc7ea7_b.jpg`,
+        image: `https://inews.gtimg.com/om_bt/OuevRi3lDJoCccAqM17UARGbNlk9CRf3pGPv7He7zA8yYAA/1000`,
         body: '时尚折边',
     },
     {
         name: 'Lanzi的敞篷纸盒',
-        image: `https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1208176849,1584967837&fm=26&gp=0.jpg`,
+        image: `https://img0.baidu.com/it/u=256816879,771155532&fm=253&fmt=auto&app=120&f=JPEG`,
+        body: '超大空间，买就送鱼罐头',
+    },
+    {
+        name: `Amy的城堡纸盒1`,
+        image: `https://inews.gtimg.com/om_bt/OjPq2cnMN_-ivDKjxpCZ2kk_ab8YC5VMnL-0pQ21fUvd4AA/1000`,
+        body: '全新, 隐蔽性好',
+    },
+    {
+        name: 'Baddy的饼干盒1',
+        image: `https://pic.rmb.bdstatic.com/bjh/914b8c0f9814b14c5fedeec7ec6615df5813.jpeg`,
+        body: '适合瘦身塑行',
+    },
+    {
+        name: 'Corney的飞行纸盒1',
+        image: `https://inews.gtimg.com/om_bt/OuevRi3lDJoCccAqM17UARGbNlk9CRf3pGPv7He7zA8yYAA/1000`,
+        body: '时尚折边',
+    },
+    {
+        name: 'Lanzi的敞篷纸盒1',
+        image: `https://img0.baidu.com/it/u=256816879,771155532&fm=253&fmt=auto&app=120&f=JPEG`,
         body: '超大空间，买就送鱼罐头',
     }
 ];
