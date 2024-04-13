7e66ad1a521e046bf5b11e2fb930cec11d2e5d79
dep <1649743146@qq.com>
2024-04-13 22:50:27 +0800

    feat: 704.二分查找

--- 算法二刷/01.二分查找.js
+++ 算法二刷/01.二分查找.js
@@ -5,6 +5,7 @@
 // 输出: -1        
 // 解释: 2 不存在 nums 中因此返回 -1       
 
+// 区间左闭右闭的情况
 function bonarySearch(nums, target) {
   let left = 0, right = nums.length - 1;
   while (left <= right) {
@@ -19,5 +20,24 @@ function bonarySearch(nums, target) {
   }
   return -1;
 }
+
+// 第二种写法，左闭右开的情况
+function bonarySearch2(nums, target) {
+  let left = 0, right = nums.length; 
+  // left == right的时候是无意义的
+  while(left < right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return -1;
+}
 console.log(bonarySearch([-1,0,3,5,9,12], 9))
 console.log(bonarySearch([-1,0,3,5,9,12], 2))
+console.log(bonarySearch2([-1,0,3,5,9,12], 9))
+console.log(bonarySearch2([-1,0,3,5,9,12], 2))
