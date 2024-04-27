5856b955c15880573cf491fd2ee77444e46fa619
dep <1649743146@qq.com>
2024-04-27 12:02:37 +0800

    feat: 59螺旋矩阵

--- 算法二刷/数组专题/14.59螺旋矩阵2.js
+++ 算法二刷/数组专题/14.59螺旋矩阵2.js
@@ -1,6 +1,40 @@
+/**
+ * 错误思路，边界条件不明确
+ * 每条边的处理规则都要相同, 遵循左闭右开的原则
+ * @param {*} n 
+ */
 function generateMatrix(n) {
-  let res = [];
-  for (let i = 0; i < n; i++) {
-    res.push(new Array(n).fill(i));
+  let res = new Array(n).fill(0).map(item => new Array(n).fill(0));
+  let loop = 1;
+  let cnt = 1;
+  let startx = 0;
+  let starty = 0;
+  while(loop <= n / 2) {
+    // 向右
+    for(let j = startx; j < n - loop; j++) {
+      res[startx][j] = cnt++;
+    }
+    // 向下
+    for(let i = starty; i < n - loop; i++) {
+      res[i][n - loop] = cnt++;
+    }
+    // 向左
+    for(let j = n - loop; j >= startx + 1; j--) {
+      res[n - loop][j] = cnt++;
+    }
+    // 向上
+    for(let i = n - loop; i >= starty + 1; i--) {
+      res[i][startx] = cnt++;
+    }
+    startx++;
+    starty++;
+    loop++;
   }
+  // 奇数
+  if(n % 2 === 1) {
+    res[startx][starty] = cnt;
+  }
+  return res;
 }
+console.log(generateMatrix(5))
+console.log(generateMatrix(4))
