266563076e518904892fad6c801c2e721f50f4e6
dep <1649743146@qq.com>
2024-05-16 23:59:37 +0800

    feat: 15

--- 算法二刷/哈希表专题/10.15三数之和.js
+++ 算法二刷/哈希表专题/10.15三数之和.js
@@ -32,3 +32,28 @@ var threeSum = function(nums) {
 console.log(threeSum([-1,0,1,2,-1,-4]));
 console.log(threeSum([0,1,1]));
 
+var sum2 = function(nums) {
+  const arr = [...nums];
+  arr.sort();
+  let res = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] > 0) break; // 说明没有符合条件的，排序后的第一个数都大于0了
+    if(i > 0 && arr[i] == arr[i - 1]) continue;
+    let left = i + 1;
+    let right = arr.length - 1;
+    while(left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      if(sum > 0) {
+        right--;
+      } else if(sum < 0) {
+        left++;
+      } else {
+        res.push([arr[i], arr[left], arr[right]]);
+        while(left < right && arr[left] == arr[left + 1]) left++;
+        while(left < right && arr[right] == arr[right - 1]) right--;
+      }
+    }
+  }
+  return res;
+}
+
