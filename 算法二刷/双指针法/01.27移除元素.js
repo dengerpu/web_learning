c753630ee33a720588357d124639860d03393bd6
dep <1649743146@qq.com>
2024-05-29 23:38:21 +0800

    feat: 27移除元素

--- 算法二刷/双指针法/01.27移除元素.js
+++ 算法二刷/双指针法/01.27移除元素.js
@@ -24,3 +24,15 @@ var removeElement = function(nums, val) {
   return nums;
 };
 console.log(removeElement([3,2,2,3], 3));
+// 双指针法
+var removeElement2 = function(nums, val) {
+  let slow = 0; fast = 0;
+  while(fast < nums.length) {
+    if(nums[fast] !== val) {
+      nums[slow++] = nums[fast++];
+    } else {
+      fast++;
+    }
+  }
+  return slow;
+}
