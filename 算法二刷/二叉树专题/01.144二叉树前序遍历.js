920f20e891b599fb94c9f426b371d09b021b88ff
dep <1649743146@qq.com>
2024-06-03 23:27:24 +0800

    144 二叉树前序遍历.js

---
+++ 算法二刷/二叉树专题/01.144二叉树前序遍历.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  if (root === null) return [];
+  let list = [];
+  // 1.确定参数和返回值
+  prevOrder(root, list);
+  return list;
+};
+
+var prevOrder = function(root, list) {
+  // 2.确定终止条件
+  if (root === null) return;
+  // 3.确定单层递归逻辑
+  list.push(root.val);
+  prevOrder(root.left, list);
+  prevOrder(root.right, list);
+}
