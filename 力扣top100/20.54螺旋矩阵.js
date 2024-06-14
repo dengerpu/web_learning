63f3c82d7fe63aaa7f445c32384ecac7e2da0022
dep <1649743146@qq.com>
2024-06-14 22:08:57 +0800

    feat: 54螺旋矩阵

--- 力扣top100/20.54螺旋矩阵.js
+++ 力扣top100/20.54螺旋矩阵.js
@@ -2,27 +2,27 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
-var spiralOrder = function(matrix) {
+var spiralOrder = function (matrix) {
     let m = matrix.length, n = matrix[0].length;
     let left = 0, right = n - 1, top = 0, bottom = m - 1;
     let res = [];
-    // 遵循左闭右开这个原则
-    while(left <= right && top <= bottom) {
+    while (left <= right && top <= bottom) {
         // 向右
-        for(let i = left; i < right; i++) {
+        for (let i = left; i <= right; i++) {
             res.push(matrix[top][i]);
         }
         // 向下
-        for(let i = top; i < bottom; i++) {
+        for (let i = top + 1; i <= bottom; i++) {
             res.push(matrix[i][right]);
         }
+        // 解决只有一行
         if (left < right && top < bottom) {
             // 向左
-            for(let i = right; i > left; i--) {
+            for (let i = right - 1 ; i > left; i--) {
                 res.push(matrix[bottom][i]);
             }
             // 向上
-            for(let i = bottom; i > top; i--) {
+            for (let i = bottom; i > top; i--) {
                 res.push(matrix[i][left]);
             }
         }
@@ -33,6 +33,3 @@ var spiralOrder = function(matrix) {
     }
     return res;
 };
-
-console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]));
-console.log(spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]));
