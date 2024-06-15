ea1fe77164623ed7a40485b9b5b27da0269d2518
dep <1649743146@qq.com>
2024-06-15 23:26:50 +0800

    feat: 160相交链表

--- 力扣top100/23.160相交链表.js
+++ 力扣top100/23.160相交链表.js
@@ -27,3 +27,37 @@ var getIntersectionNode = function(headA, headB) {
   // 即使不相交，最后也都会为null
   return pA;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+ // 利用哈希集合来存储
+ var getIntersectionNode = function(headA, headB) {
+  if(headA == null || headB == null) {
+      return null;
+  }
+  let set = new Set();
+  let p = headA;
+  while(p) {
+      set.add(p);
+      p = p.next;
+  }
+  p = headB;
+  while(p) {
+      if(set.has(p)) {
+          return p;
+      }
+      p = p.next;
+  }
+  return null;
+};
