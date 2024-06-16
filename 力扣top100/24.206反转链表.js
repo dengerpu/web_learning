db7ce2aade166a05bac5b86784f4dc259d0a80ec
dep <1649743146@qq.com>
2024-06-16 20:55:35 +0800

    feat: 206反转链表-递归法

--- 力扣top100/24.206反转链表.js
+++ 力扣top100/24.206反转链表.js
@@ -21,3 +21,11 @@ var reverseList = function(head) {
   }
   return pre;
 };
+// 递归法
+var reverseList = function(head) {
+  if(head == null || head.next == null) return head;
+  let newHead = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return newHead;
+};
