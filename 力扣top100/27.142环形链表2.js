ca31bad991a18b8b1583e39a2168c07d36fa90f5
dep <1649743146@qq.com>
2024-06-18 23:46:27 +0800

    feat: 环形链表2-双指针

--- 力扣top100/27.142环形链表2.js
+++ 力扣top100/27.142环形链表2.js
@@ -11,16 +11,39 @@
  * @return {ListNode}
  */
  // 哈希表
- var detectCycle = function(head) {
+// var detectCycle = function(head) {
+//     if(head == null || head.next == null) return null;
+//     let set = new Set();
+//     let p = head;
+//     while(p) {
+//         if(set.has(p)) {
+//             return p;
+//         }
+//         set.add(p);
+//         p = p.next;
+//     }
+//     return null;
+// };
+
+// 双指针法
+var detectCycle = function(head) {
   if(head == null || head.next == null) return null;
-  let set = new Set();
-  let p = head;
-  while(p) {
-      if(set.has(p)) {
-          return p;
+  let slow = head, fast = head;
+  while(fast) {
+      slow = slow.next;
+      if(fast.next != null) {
+          fast = fast.next.next;
+      } else {
+          return null;
       }
-      set.add(p);
-      p = p.next;
+      if(slow == fast) { // 两者相交，但这个交点不一定是环形链表的入口
+          let p = head;
+          while(p != slow) {
+              p = p.next;
+              slow = slow.next
+          }
+          return p;
+      } 
   }
   return null;
 };
