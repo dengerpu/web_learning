8e141114303fab12c4ab8a4bf7ed20def25b32c2
dep <1649743146@qq.com>
2024-06-27 23:54:33 +0800

    feat: 二叉树的最大深度-广度优先遍历

--- 力扣top100/33.104二叉树的最大深度.js
+++ 力扣top100/33.104二叉树的最大深度.js
@@ -10,8 +10,30 @@
  * @param {TreeNode} root
  * @return {number}
  */
- // 深度优先搜索
+ // 深度优先遍历
 var maxDepth = function(root) {
     if(root == null) return 0;
     return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+};
+
+// 广度优先搜索
+var maxDepth = function(root) {
+  if(root == null) return 0;
+  let queue = [];
+  queue.push(root);
+  let length = 0;
+  while(queue.length > 0) {
+      let len = queue.length
+      for(let i = 0; i < len; i++) {
+          let curr = queue.shift();
+          if(curr.left) {
+              queue.push(curr.left);
+          }
+          if(curr.right) {
+              queue.push(curr.right);
+          }
+      }
+      length++;
+  }
+  return length;
 };
