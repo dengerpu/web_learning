13ce894b7d1b034e040de5134618308394202a3e
dep <1649743146@qq.com>
2024-07-02 23:57:47 +0800

    feat: 二叉树的层次遍历

---
+++ 力扣top100/37.102二叉树的层次遍历.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  let res = [];
+  if(!root) return res;
+  let queue = [root];
+  while(queue.length){
+    let len = queue.length;
+    let level = [];
+    while(len--){
+      let node = queue.shift();
+      level.push(node.val);
+      if(node.left) queue.push(node.left);
+      if(node.right) queue.push(node.right);
+    }
+    res.push(level);
+  }
+  return res;
+};
