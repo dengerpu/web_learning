9d41c32ac0ef25e2e0bc8009aa1598ccc3b8df02
dep <1649743146@qq.com>
2024-07-12 22:58:42 +0800

    feat: 78子集

--- 力扣top100/43.78子集.js
+++ 力扣top100/43.78子集.js
@@ -5,19 +5,16 @@
 let res = [];
 let path = [];
 var subsets = function(nums) {
-   res = [];
-   path = [];
-   backTrack(nums, 0, path);
+   let res = [];
+   let path = [];
+   function backtracking(startIndex) {
+       res.push([...path]);
+       for(let i = startIndex; i < nums.length; i++) {
+           path.push(nums[i]);
+           backtracking(i+1);
+           path.pop();
+       }
+   }
+   backtracking(0);
    return res;
 };
-var backTrack = function(nums, i, path) {
-   if(path.length <= nums.length) {
-       res.push([...path]);
-       return;
-   }
-   for(let start = i; start < nums.length; start++) {
-       path.push(nums[i]);
-       backTrack(nums, start + 1, path);
-       path.pop();
-   }
-}
