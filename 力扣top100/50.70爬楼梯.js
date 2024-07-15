fc77584e47af6788a0c045cdab80ba1f3aeb7ea5
dep <1649743146@qq.com>
2024-07-15 23:55:35 +0800

    feat: 70爬楼梯

---
+++ 力扣top100/50.70爬楼梯.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let p = 0, q = 0, r = 1;
+    for(let i = 0; i < n; i++) {
+        p = q;
+        q = r;
+        r = p + q;
+    }
+    return r;
+};
+console.log('爬楼梯', climbStairs(3))
