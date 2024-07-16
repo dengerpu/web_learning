c5888e7390f7d544a289be065339c4b0ae3288ab
dep <1649743146@qq.com>
2024-07-16 23:59:41 +0800

    feat: 121买卖股票最佳时机

---
+++ 力扣top100/51.121买卖股票最佳时机.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = 0;
+    let maxProfit = Infinity;
+    for(let i = 0; i < prices.length; i++) {
+        
+    }
+};
