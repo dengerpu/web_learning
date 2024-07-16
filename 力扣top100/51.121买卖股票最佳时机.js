c56377d8d8dd1f817f5e8f7c953e967461efcbb5
dep <1649743146@qq.com>
2024-07-17 00:03:20 +0800

    feat: 121买卖股票的最佳时机

--- 力扣top100/51.121买卖股票最佳时机.js
+++ 力扣top100/51.121买卖股票最佳时机.js
@@ -3,9 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minPrice = 0;
-    let maxProfit = Infinity;
+    let minPrice = Infinity;
+    let maxProfit = 0;
     for(let i = 0; i < prices.length; i++) {
-        
+        if(prices[i] < minPrice) {
+            minPrice = prices[i];
+        }else {
+            if(prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
     }
+    return maxProfit;
 };
