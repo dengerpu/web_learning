980cb6c613df70a63a056bc991205ab0ec1f562f
dengerpu <dengerpu@kuaishou.com>
2024-08-13 09:51:10 +0800

    feat: 处理高并发

--- github手写题/2.promise周边场景题/22.处理高并发.js
+++ github手写题/2.promise周边场景题/22.处理高并发.js
@@ -1,6 +1,29 @@
-1111+
-222
-333
-444
-test
+async function asyncPool(poolLimit, tasks) {
+    const results = [];
+    const poolList = new Set();
 
+    for(const task of tasks) {
+        if(poolList.size === poolLimit) {
+            await Promise.race(poolList).catch(err => console.log(err));
+        }
+        const p = task();
+        const cb = () => poolList.delete(p);
+        poolList.add(p);
+        results.push(p);
+    }
+    return Promise.allSettled(results);
+}
+
+function task() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const timestamp = Date.now() / 1000 | 0
+        console.log(timestamp)
+        resolve(timestamp)
+      }, 1000)
+    })
+}
+asyncPool(2, [task, task, task, task, task, task, task, task, task])
+  .then(res => {
+    console.log(res)
+  });
